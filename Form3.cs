efa42f54e05c7445b49ce34faf2a1040d1da6e0f
Łukasz Śladowski <l.sladowski1@gmail.com>
2023-03-05 23:13:31 +0100

    Ostateczny projekt(Jeden Krok do końca...)

--- Form3.cs
+++ Form3.cs
@@ -111,63 +111,8 @@ namespace Czytnik_kodu_genetycznego
             return 0;
         }
 
-        void Przewijaniezdjecia()
-        {
-
-
-
-
-        }
-
         int currentImageIndex = 0;
         string imagesFolderPath = "./Assets/Obrazki/";
-
-        int[] IlosciKodonow = new int[22];
-
-        void LiczenieKodonow(int NumerListy)
-        {
-            int[] wynik = new int[20];
-
-            for(int s= 0; s<Form1.ListaBialek.Length;s++)
-            {
-                for (int j = 0; j < Form1.BialkaWyjsciowe0[NumerListy].Length; j++)
-                {
-                    for (int w = 0; w < IlosciKodonow.Length; w++)
-                    {                    
-                        if (IlosciKodonow[w].Equals(Form1.ListaBialek[s]))
-                        {
-                            wynik[s] += 1;
-                        }
-                    }
-                }
-                Console.WriteLine("\nW tym " + wynik[s] + " elementy o wartości " + Form1.ListaBialek[s]);
-            }
-            //for (int j = 0; j < Form1.BialkaWyjsciowe0[NumerListy].Length; j++)
-            //{
-            //}
-
-
-
-            //for (int i = 0; i < IlosciKodonow.Length; i++)
-            //{
-            //    for (int j = 0; j < Form1.BialkaWyjsciowe0[NumerListy].Length; j++)
-            //    {
-            //        Console.WriteLine(Form1.ListaBialek[i] +" :TO A TO: "+ Form1.BialkaWyjsciowe0[j]);
-            //        IlosciKodonow[i]++;
-            //        if (Form1.ListaBialek[i] == Form1.BialkaWyjsciowe0[j])
-            //        {
-            //            IlosciKodonow[i]++;
-
-
-
-            //        }
-            //    }
-            //    //Console.WriteLine(IlosciKodonow[i]);
-            //}
-
-            //return 0;
-
-        }
 
         private void Form3_Load(object sender, EventArgs e)
         {
@@ -182,15 +127,6 @@ namespace Czytnik_kodu_genetycznego
             Console.WriteLine(Form1.BialkaWyjsciowe0[1]);
             Console.WriteLine(Form1.BialkaWyjsciowe0[2]);
 
-            LiczenieKodonow(0);
-            Console.WriteLine("========");
-            LiczenieKodonow(1);
-            Console.WriteLine("========");
-            LiczenieKodonow(2);
-
-            //label1.Text = "AAAA " + LiczenieKodonow(0);
-            //label2.Text = "AAAA " + LiczenieKodonow(1);
-            //label3.Text = "AAAA " + LiczenieKodonow(2);
             Console.WriteLine("\n" + Form1.BialkaWyjsciowe0[0]);
             Console.WriteLine(Form1.BialkaWyjsciowe0[1]);
             Console.WriteLine(Form1.BialkaWyjsciowe0[2]);
@@ -214,96 +150,3 @@ namespace Czytnik_kodu_genetycznego
         }
     }
 }
-/*
-Form1.BialkaWyjsciowe - ta tabela.
-string[] obrazki = new string[Form1.BialkaWyjsciowe.length];
-int i = 0;
-while (i < Form1.BialkaWyjsciowe.length) {
-switch (Form1.BialkaWyjsciowe[i]) {
-        case "A":
-            obrazk1[i] = "Alanine";
-            i = i+1;
-            break;
-        case "R":
-            obrazki[i] = Arginine;
-            i = i+1;
-            break;
-        case "N":
-            obrazki[i] = Asparagine;
-            i = i+1;
-            break;
-        case "D":
-            obrazki[i] = Aspartic Acid;
-            i = i+1;
-            break;
-        case "C":
-            obrazki[i] = Crysteine;
-            i = i+1;
-            break;
-	    case "E":
-            obrazk1[i] = Glutamic Acid;
-            i = i+1;
-            break;
-        case "Q":
-            obrazki[i] = Glutamine;
-            i = i+1;
-            break;
-        case "G":
-            obrazki[i] = Glycine;
-            i = i+1;
-            break;
-        case "H":
-            obrazki[i] = Histidnine;
-            i = i+1;
-            break;
-        case "I":
-            obrazki[i] = Isoleucine;
-            i = i+1;
-            break;
-	    case "L":
-            obrazk1[i] = Leucine;
-            i = i+1;
-            break;
-        case "K":
-            obrazki[i] = Lysine;
-            i = i+1;
-            break;
-        case "M":
-            obrazki[i] = Methionine;
-            i = i+1;
-            break;
-        case "F":
-            obrazki[i] = Phenylalanine;
-            i = i+1;
-            break;
-        case "P":
-            obrazki[i] = Proline;
-            i = i+1;
-            break;
-	    case "U":
-            obrazk1[i] = Selenofysteine;
-            i = i+1;
-            break;
-        case "S":
-            obrazki[i] = Serine;
-            i = i+1;
-            break;
-        case "T":
-            obrazki[i] = Theonine;
-            i = i+1;
-            break;
-        case "W":
-            obrazki[i] = Tryptophan;
-            i = i+1;
-            break;
-        case "Y":
-            obrazki[i] = Tyrosine;
-            i = i+1;
-            break;
-	    case "V":
-            obrazki[i] = Valine;
-            i = i+1;
-            break;
-	}
-}
-*/
